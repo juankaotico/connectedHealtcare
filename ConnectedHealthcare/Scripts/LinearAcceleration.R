b74c504f73af0d831fab92b89e67a86dc9fda521
jcremerius <jonas.cremerius@student.hpi.de>
2019-05-06 11:02:31 +0200

    Add script for each measure

---
+++ ConnectedHealthcare/Scripts/LinearAcceleration.R
@@ -0,0 +1,7 @@
+library(readr)
+log <- read_csv("Scripts/log.txt")
+sensor_name <- "Linear Acceleration"
+plot(log$value1[log$sensorName == sensor_name], ylab = sensor_name)
+lines(log$value2[log$sensorName == sensor_name], type='l', col="red")
+lines(log$value3[log$sensorName == sensor_name], type='l', col="blue")
+legend("topleft", c("value1", "value2", "value3"), col = c("black", "red", "blue"), lty = 1, cex = 0.8)
--- ConnectedHealthcare/loadData.R
+++
@@ -1,5 +0,0 @@
-library(readr)
-log <- read_csv("log.txt")
-log
-plot(log$value1[log$sensorName == 'Gravity'], log$value2[log$sensorName == 'Gravity'])
-
